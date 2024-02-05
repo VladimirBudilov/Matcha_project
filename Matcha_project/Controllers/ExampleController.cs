249a9333be3d69751185739b8afe9e47917e830c
Vladimir <v.ladimirbudilov@yandex.ru>
2024-02-05 23:00:13 +0400

    add asp.net mvc project

---
+++ Matcha_project/Controllers/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿// Controllers/ExampleController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace Matcha_project.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This is an example message.";
+            return View();
+        }
+        
+        public void ExampleMethod()
+        {
+            Console.WriteLine("This is an example method.");
+        }
+    }
+}
