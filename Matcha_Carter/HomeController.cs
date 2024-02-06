eb58f3cd4d1677c3fe907d7325507c6e4707c64e
Vladimir <v.ladimirbudilov@yandex.ru>
2024-02-06 22:57:12 +0400

    add carter

---
+++ Matcha_Carter/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace CarterAndMVC
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET
+        public string Index()
+        {
+            return "Hello from MVC";
+        }
+    }
+}
---
+++ Matcha_Carter/HomeModule.cs
@@ -0,0 +1,11 @@
+﻿namespace CarterAndMVC
+{
+    using Carter;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Routing;
+
+    public class HomeModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app) => app.MapGet("/", () => "Hello from Carter!");
+    }
+}
