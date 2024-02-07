c6092794d06e08f73dbd228c3e3ffb226f512cf8
Vladimir <v.ladimirbudilov@yandex.ru>
2024-02-07 21:57:19 +0400

    add cors

--- Matcha_Carter/Program.cs
+++ Matcha_Carter/Program.cs
@@ -1,24 +1,38 @@
+using System;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using CarterAndMVC.Database;
 using Carter;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var dbPath = Environment.GetEnvironmentVariable("DB_PATH");
+var baseUrl = Environment.GetEnvironmentVariable("BASE_URL");
+var frontUrl = Environment.GetEnvironmentVariable("FRONT_URL");
+// print dbPath and baseUrl
+Console.WriteLine($"DB_PATH: {dbPath}");
+Console.WriteLine($"BASE_URL: {baseUrl}");
+Console.WriteLine($"FRONT_URL: {frontUrl}");
 builder.Host.UseContentRoot(Directory.GetCurrentDirectory());
 
 builder.Services.AddCarter();
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder => builder.WithOrigins(frontUrl).AllowAnyHeader().AllowAnyMethod());
+});
 builder.Services.AddSingleton<SQLiteDbService>(
     new SQLiteDbService(
-        $"Data Source={Path.Combine(Directory.GetCurrentDirectory(), "../Matcha_Data/Matcha_db.sqlite")};Version=3;"));
+        $"Data Source={Path.Combine(Directory.GetCurrentDirectory(), dbPath)};Version=3;"));
 
 var app = builder.Build();
 app.UseDefaultFiles();
 app.UseRouting();
 app.MapControllers();
 app.MapCarter();
-app.Run();
+app.UseCors();
+app.Run(baseUrl);
