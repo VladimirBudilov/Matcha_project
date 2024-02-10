2e523a74f5d4f45162b1b99066ffaada035b8675
mansur0987-del <85795437+mansur0987-del@users.noreply.github.com>
2024-02-10 21:09:03 +0600

    sign up button

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -19,6 +19,7 @@
   const onFinishFailed = (errorInfo: any) => {
 	console.log('Failed:', errorInfo);
   };
+
 </script>
 
 
@@ -54,6 +55,11 @@
 		</a-form-item>
 		</a-form>
 	</div>
+	<div class="SignUpButton">
+		<a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+			<a-button type="primary" html-type="submit">Sign up</a-button>
+		</a-form-item>
+	</div>
 </template>
 
 <style>
@@ -62,16 +68,17 @@
 	top: 30vh;
 }
 
+:where(.css-dev-only-do-not-override-1hsjdkk).ant-form-item .ant-form-item-control-input {
+	width: 25vw;
+}
+
 :where(.css-dev-only-do-not-override-1hsjdkk).ant-form-item .ant-form-item-label >label {
 	color: var(--color-text);
 }
 
-:where(.css-dev-only-do-not-override-1hsjdkk).ant-input {
-	width: 25vw;
-}
-
-:where(.css-dev-only-do-not-override-1hsjdkk).ant-col-16 {
-	width: 25vw;
+.SignUpButton {
+	position: relative;
+	top: 28vh;
 }
 
 </style>
