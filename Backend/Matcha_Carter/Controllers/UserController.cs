6c6ba6a6accb709835cb05edb016db6c4fbb2b09
Vladimir <v.ladimirbudilov@yandex.ru>
2024-04-08 21:57:29 +0400

    add a mess in the backend folder)

---
+++ Backend/Matcha_Carter/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Matcha_Carter.Controllers
+{
+    [Route("user")]
+    internal class UserController : Controller
+    {
+        public UserController() { }
+
+
+        [HttpGet]
+        [Route("/{id}")]
+        public IActionResult GetUsers(Guid id)
+        {
+            return Ok("get user with id");
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok("get all users");
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser()
+        {
+            return Ok("create user");
+        }
+
+        [HttpPut]
+        [Route("/{id}")]
+        public IActionResult UpdateUser(Guid id)
+        {
+            return Ok("update user with id");
+        }
+
+        [HttpDelete]
+        [Route("/{id}")]
+        public IActionResult DeleteUser(Guid id)
+        {
+            return Ok("delete user with id");
+        }
+    }
+}
